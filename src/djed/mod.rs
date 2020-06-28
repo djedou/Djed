6db13c847249a71b8597d7c804bb0b6aa291e8c3
djedou <djedou@localhost.localdomain>
2020-06-28 18:09:40 +0200

    fixing should render

--- src/djed/mod.rs
+++ src/djed/mod.rs
@@ -4,7 +4,7 @@ mod djed;
 
 pub use djed::{
     ChildrenRenderer, Component, Html, Children, ChildrenWithProps, NodeRef, Renderable, Props,
-    EmptyBuilder, ComponentLink, Href
+    EmptyBuilder, ComponentLink, Href, ShouldRender
 };
 pub use scope::{
     AnyScope, Scope, ComponentUpdate, Scoped
